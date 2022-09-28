57804708dc8a653520523286343e7535a4eb8a38
Sam Oakes <the.spiderminecart.0@gmail.com>
2022-09-27 20:46:51 -0700

    centre

--- src/actial/tilesys.java
+++ src/actial/tilesys.java
@@ -1,6 +1,9 @@
 package actial;
+
 import java.util.ArrayList;
 import java.util.HashMap;
+
+import static com.raylib.Raylib.*;
 
 import tiles.tile;
 
@@ -11,18 +14,21 @@ public class tilesys {
     public ArrayList<String> tls;
     private int size;
 
-
     public void draw(renderer context) {
         for (int i = 0; i < width; i++) {
             for (int j = 0; j < width; j++) {
-                tiles.get("dirt").draw(context, j*(size/2)-(i*size/2),(i)*(size/4)+(j*(size/4)));
+                // j*s/2-(i*s/2)
+                // i*s/4+(j*(s/4))
+                tiles.get("dirt").draw(context, ((j * (size / 2) - (i * size / 2)) - size / 2) + GetScreenWidth() / 2,
+                        ((i * (size / 4) + (j * (size / 4))))
+                                + ((GetScreenHeight() / 2) - ((width * (size / 2)) / 2 + size / 2)));
             }
         }
     }
 
     public void resize(renderer context, int s) {
         size = s;
-        tiles.forEach((k,v) -> v.resize(context, s));
+        tiles.forEach((k, v) -> v.resize(context, s));
     }
 
     public void regis(String name, tile inst) {
@@ -30,11 +36,11 @@ public class tilesys {
     }
 
     public void save(savobj save) {
-        //TODO: save
+        // TODO: save
     }
 
     public savobj load() {
-        //TODO: load
+        // TODO: load
         return new savobj();
     }
 }
